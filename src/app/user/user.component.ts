852884e091b5bbaf5173df2ffa48542bb0cb6d80
onurabdulaji <onurabdulaji@gmail.com>
2025-05-16 14:16:04 +0200

    Defining Component Inputs

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -11,13 +11,12 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  @Input() avatar!: string;
+  @Input() name!: string;
 
   get ImagePath() {
-    return 'assets/users/' + this.selectedUser.avatar;
+    return 'assets/users/' + this.avatar;
   }
 
-  onSelectUser() {
-    console.log('Clicked');
-  }
+  onSelectUser() {}
 }
