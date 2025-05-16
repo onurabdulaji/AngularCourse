eab1465ce6fe1adf3a14d2ed8f9e5fae5c6d0fb4
onurabdulaji <onurabdulaji@gmail.com>
2025-05-16 17:50:29 +0200

    Getting Started with Services

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -14,37 +14,12 @@ export class TasksComponent {
   @Input({ required: true }) userid!: string;
   @Input({ required: true }) name!: string;
   isAddingTask = false;
-  tasks = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master Angular',
-      summary: 'Learn the basics of Angular and build a simple application.',
-      dueDate: '2023-10-01',
-    },
-    {
-      id: 't2',
-      userId: 'u3',
-      title: 'Master Angular',
-      summary: 'Learn the basics of Angular and build a simple application.',
-      dueDate: '2023-10-01',
-    },
-    {
-      id: 't3',
-      userId: 'u3',
-      title: 'Master Angular',
-      summary: 'Learn the basics of Angular and build a simple application.',
-      dueDate: '2023-10-01',
-    },
-  ];
 
   get selectedUserTasks() {
-    return this.tasks.filter((task) => task.userId === this.userid);
+    return;
   }
 
-  onCompleteTask(id: string) {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
-  }
+  onCompleteTask(id: string) {}
 
   onStartAddTask() {
     this.isAddingTask = true;
@@ -55,13 +30,6 @@ export class TasksComponent {
   }
 
   onAddTask(taskData: newTaskData) {
-    this.tasks.unshift({
-      id: new Date().getTime().toString(),
-      title: taskData.title,
-      summary: taskData.summary,
-      dueDate: taskData.date,
-      userId: this.userid,
-    });
     this.isAddingTask = false;
   }
 }
---
+++ src/app/tasks/tasks.service.ts
@@ -0,0 +1,45 @@
+import { type newTaskData } from './task/task.model';
+
+class TasksService {
+  private tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary: 'Learn the basics of Angular and build a simple application.',
+      dueDate: '2023-10-01',
+    },
+    {
+      id: 't2',
+      userId: 'u3',
+      title: 'Master Angular',
+      summary: 'Learn the basics of Angular and build a simple application.',
+      dueDate: '2023-10-01',
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Master Angular',
+      summary: 'Learn the basics of Angular and build a simple application.',
+      dueDate: '2023-10-01',
+    },
+  ];
+
+  getUserTaks(userId: string) {
+    return this.tasks.filter((task) => task.userId === userId);
+  }
+
+  addTask(taskData: newTaskData, userId: string) {
+    this.tasks.unshift({
+      id: new Date().getTime().toString(),
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.date,
+      userId: userId,
+    });
+  }
+
+  removeTask(id: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+}
