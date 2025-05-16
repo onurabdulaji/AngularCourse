29b3b26bc8a04d57f8bd41687cfc3f9ac16a7108
onurabdulaji <onurabdulaji@gmail.com>
2025-05-16 14:48:46 +0200

    Using the output() Function

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  input,
+  Output,
+  output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-user',
