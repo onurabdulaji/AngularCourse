26c84a00661021fc833cbb317b3e759289d6161f
onurabdulaji <onurabdulaji@gmail.com>
2025-05-16 14:33:39 +0200

    Using Signal Inputs

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,4 @@
 import { Component, Input } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
